1483bd82b9b3ab435515565530e2ce57bf9681c9
xiong <403038469@qq.com>
2025-05-15 17:12:52 +0800

    自动评测各学习率下的accuracy

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torch.distributed as dist
 import torchvision
 import torch
 
+from tqdm import tqdm
 from torch.optim import Optimizer
 from torchvision.transforms import ToTensor
 from torch.utils.data import random_split, DataLoader
@@ -13,9 +14,9 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from dataloader import *
 from utils import *
 
-def do_train(train_loader, loss_fn, avail_device, optimizer_mode='sgd', epochs=10, lr=0.01):
+def do_train(train_loader, test_loader, loss_fn, avail_device, optimizer_mode='sgd', epochs=10, lr=0.01):
     
-    model = ClassificationNet(input_shape=3, num_classes=10).to(avail_device)
+    model = ClassificationNet(input_channels=3, num_classes=10).to(avail_device)
     print(model)
     print(f"Model parameters: {sum(p.numel() for p in model.parameters())}")
 
@@ -30,6 +31,7 @@ def do_train(train_loader, loss_fn, avail_device, optimizer_mode='sgd', epochs=1
 
     model.train()
     loss_record = []
+    epoch_loss = 0
 
     for epoch in range(epochs):
         for batch_idx, (train_img, train_lbl) in enumerate(train_loader):
@@ -40,21 +42,26 @@ def do_train(train_loader, loss_fn, avail_device, optimizer_mode='sgd', epochs=1
             optimizer.zero_grad()
             y_pred = model(train_img)
             loss = loss_fn(y_pred, train_lbl)
-            loss_record.append(loss.detach().item())
+            epoch_loss += loss.detach().item()
             loss.backward()
             optimizer.step()
 
+        loss_record.append(epoch_loss / len(train_loader))
+        epoch_loss = 0
+
         if (epoch+1) % 5 == 0:
-            print(f"Epoch {epoch+1}, Loss: {loss.item()}, Optimizer: {optimizer_mode}, LR: {lr}")
+            print(f"Epoch {epoch+1}, Loss: {loss_record[-1]}, Optimizer: {optimizer_mode}, LR: {lr}")
+    
+    accuracy_record = model_acc(model, test_loader, avail_device)
 
-    return model, loss_record
+    return model, loss_record, accuracy_record
 
-def main(batch_size=64):
+def main(epoch = 30, batch_size=64, optimizer='sgd'):
 
     transform = ToTensor()
 
-    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
+    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
 
     avail_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {avail_device}")
@@ -69,25 +76,39 @@ def main(batch_size=64):
         '0.0005': [],
         '0.0001': [],
     }
+
+    acc_record = {
+        '0.001': [],
+        '0.0005': [],
+        '0.0001': [],
+    }
     
     # loss_fn = nn.MSELoss()
     loss_fn = nn.CrossEntropyLoss()
     
     for lr in loss_record.keys():
         print(f"Training with learning-rate: {lr}")
-        model, record_loss = do_train(
-                                    train_loader, 
-                                    loss_fn, 
-                                    avail_device, 
-                                    optimizer_mode='sgd',  # 使用不同的优化器
-                                    epochs=100,
-                                    lr=float(lr)  # 使用不同的学习率
-                                )
+        model, record_loss, record_acc = do_train(
+                                        train_loader, 
+                                        test_loader,
+                                        loss_fn, 
+                                        avail_device, 
+                                        optimizer_mode=optimizer,  # 不同的优化器: sgd, adam, adagrad
+                                        epochs=epoch,
+                                        lr=float(lr)
+                                    )
+        
         loss_record[lr] = record_loss
+        acc_record[lr] = record_acc
+
         plot_loss_comparison(
                             loss_record,
-                            title=f"Loss Comparison with SGD", 
+                            title=f"Loss Comparison with {optimizer}", 
                         )
+        plot_accuracy_comparison(
+                                acc_record,
+                                title=f"Accuracy Comparison with {optimizer}",
+                            )
 
     print("Training completed for all optimizers.")
 
@@ -96,5 +117,7 @@ if __name__ == "__main__":
     torch.manual_seed(42)
 
     main(
-        batch_size=64
+        epoch=30,
+        batch_size=256,
+        optimizer='sgd'
     )
--- utils.py
+++ utils.py
@@ -73,7 +73,7 @@ class MyOptimizer(torch.optim.Optimizer):
     """
         Parameters:
             params: 可迭代的参数列表
-            mode: 优化器模式（'gd' 或 'sgd'）
+            mode: 优化器模式（'sgd', 'adagrad' 或 'adam'）
             lr: 学习率
             momentum: 动量（仅适用于 'sgd' 模式）
             betas: Adam优化器的beta参数（仅适用于 'adam' 模式）
@@ -123,3 +123,53 @@ def plot_loss_comparison(
     plt.yscale('log')  # Log scale for better visualization of differences
     plt.tight_layout()
     plt.savefig("loss_comparison.png")
+
+def plot_accuracy_comparison(
+                        accuracy_records,
+                        title='Accuracy Curves Comparison'
+                    ):
+    
+    plt.figure(figsize=(10, 6))
+    for mode, accuracy_record in accuracy_records.items():
+        plt.plot(accuracy_record, label=mode)
+    
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title(title)
+    plt.legend()
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.tight_layout()
+    plt.savefig("accuracy_comparison.png")
+
+def model_acc(model, test_loader, avail_device):
+
+    epoch_accuracy = 0
+    accuracy_record = []
+
+    model.eval()
+    with torch.no_grad():
+        total_correct = 0
+        total_samples = 0
+
+        for batch_idx, (test_img, test_lbl) in enumerate(test_loader):
+            test_img = test_img.to(avail_device)
+            test_lbl = test_lbl.to(avail_device)
+
+            y_pred = model(test_img)
+            _, predicted = torch.max(y_pred.data, 1)
+
+            # 计算当前 batch 的正确预测数和总样本数
+            correct = (predicted == test_lbl).sum().item()
+            total = test_lbl.size(0)
+
+            total_correct += correct
+            total_samples += total
+
+        # 计算整体准确率
+        epoch_accuracy = total_correct / total_samples
+        accuracy_record.append(epoch_accuracy)
+
+        print(f"Test Accuracy: {epoch_accuracy:.4f}")
+        
+    
+    return accuracy_record
